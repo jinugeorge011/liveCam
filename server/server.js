fcea5c95d50c6f6d33a6b20d91cce9afc00cdad4
jinugeorge011 <jinugeorge8787@gmail.com>
2024-12-09 12:25:30 +0530

    Updated

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ["GET", "POST"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
 
