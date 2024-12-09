f96c007244c1a31dc1b4026ccf5603896e3f4eb0
jinugeorge011 <jinugeorge8787@gmail.com>
2024-12-09 16:07:04 +0530

    Updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import DarkModeSwitcher from './DarkModeSwitcher';
 import logomain from '../Images/Logo-Main.png'
+import Chatbot from './ChatBot';
 
 
 function Header({ users }) {
@@ -91,7 +92,9 @@ function Header({ users }) {
   </NavLink>
 )}
 
-
+<div>
+  <Chatbot/>
+</div>
         {/* Dark Mode Switcher */}
         <div className="mt-1">
           <DarkModeSwitcher />
