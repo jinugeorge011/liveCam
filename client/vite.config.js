1d6a8a9e6ce0813378c9e6a20e272d6a1b35b1b7
jinugeorge011 <jinugeorge8787@gmail.com>
2024-12-10 22:50:01 +0530

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,4 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ plugins: [react()], server: { proxy: { '/api': { target: 'http://localhost:5000', changeOrigin: true, rewrite: (path) => path.replace(/^\/api/, '') } } } });
+export default defineConfig({ plugins: [react()], server: { proxy: { '/api': { target: 'https://livecam-7fzf.onrender.com', changeOrigin: true, rewrite: (path) => path.replace(/^\/api/, '') } } } });
